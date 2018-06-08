e91755c37e94a27680981d8efdc427ce04e44f03
Jude Beadle <info@jbeadle.de>
2018-06-08 16:35:06 +0200

    setting up eventlistener

--- js/catclicker.js
+++ js/catclicker.js
@@ -1,2 +1,12 @@
-const container = document.getElementsByClassName('container');
-const catImage = document.getElementsByClassName('cat-image');
+window.addEventListener('load',function(){
+
+    const container = document.querySelector('.container');
+    const catImage = container.querySelector('img');
+    
+    let numClicks = 0;
+
+	catImage.addEventListener('click', function () {
+	    numClicks ++;
+	    console.log(numClicks)
+	}, false);
+});
