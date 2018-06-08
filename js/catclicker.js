bc40b3015511bbea87d8b07a15013c2edd20f2b9
Jude Beadle <info@jbeadle.de>
2018-06-08 16:36:33 +0200

    adding click counter

--- js/catclicker.js
+++ js/catclicker.js
@@ -2,11 +2,17 @@ window.addEventListener('load',function(){
 
     const container = document.querySelector('.container');
     const catImage = container.querySelector('img');
-    
+    const clickCounter = document.createElement('p');
+    container.append(clickCounter);
     let numClicks = 0;
 
 	catImage.addEventListener('click', function () {
 	    numClicks ++;
-	    console.log(numClicks)
+		updateCounter(numClicks);
 	}, false);
+
+	function updateCounter(numClicks){
+		let counterText = 'you clicked that cat ' + numClicks + ' times!'
+		clickCounter.innerHTML = counterText;
+	}
 });
